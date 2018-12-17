3e3a3c3cf20234f1535cf94b8b6ed9c7c3c36e40
imtoshi01 <imtoshi01@gmail.com>
2018-12-17 23:10:12 +0530

    Update HiringApplication.java

--- webpage/src/main/java/com/hiringcupid/webpage/HiringApplication.java
+++ webpage/src/main/java/com/hiringcupid/webpage/HiringApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HiringApplication {
-
+	//main
 	public static void main(String[] args) {
 		SpringApplication.run(HiringApplication.class, args);
 	}
